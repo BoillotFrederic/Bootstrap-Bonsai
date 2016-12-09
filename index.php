5cb0f3132bb383a641c37acc44fad6c9564e6d01
BoillotFrederic <boillot.frederic.62@gmail.com>
2016-12-09 10:36:15 +0100

    Bootstrap-Bonsai

---
+++ index.php
@@ -0,0 +1,182 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Bootstrap-Bonsai</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/style.css">
+  </head>
+  <body>
+    <div id="logo" class="container-fluid">
+      <nav class="navbar navbar-default">
+        <div class="container-fluid">
+          <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+              <span class="sr-only">Toggle navigation</span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#"><span>B</span> BONSAI</a>
+          </div>
+          <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="nav navbar-nav navbar-right">
+              <li><a href="#">LOGIN</a></li>
+              <li><a href="#">SIGN UP</a></li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+      <div id="title" class="text-center">
+        <h1>
+          Simple &amp; Beautiful Payments For Freelancers<br />
+          <small>Get paid on time, every time.</small><br />
+          <button class="btn btn-default">CREATE A FREE ACCOUNT</button>
+        </h1>
+      </div>
+    </div>
+    <div id="access" class="container-fluid text-center">
+      <div class="row">
+        <div class="col-xs-12 col-sm-4 col-md-4 col-lg-4">
+          <img src="imgs/payments-calendar-icon.svg" alt="Payments" /><br />
+          Quick<br />
+          <small>to create, send, and pay invoices</small>
+        </div>
+        <div class="col-xs-12 col-sm-4 col-md-4 col-lg-4">
+          <img src="imgs/payments-mail-icon.svg" alt="Payments" /><br />
+          Beautiful<br />
+          <small>process for the freelancer and client</small>
+        </div>
+        <div class="col-xs-12 col-sm-4 col-md-4 col-lg-4">
+          <img src="imgs/payments-methods-icon.svg" alt="Payments" /><br />
+          Secure<br />
+          <small>payments and quick disbursement</small>
+        </div>
+      </div>
+    </div>
+    <div id="payInfo" class="container-fluid text-center">
+      <div class="row">
+        SIMPLE PRICING:<br />
+        PAY WHEN YOU GET PAID<br />
+        <br />
+        <h2><span>$1</span>PER PAID INVOICE<br /><small>2.9% &amp; 30&cent; transaction fee</small></h2>
+      </div>
+    </div>
+    <div id="creditInfo" class="container-fluid text-center">
+      Credit card supported now. ACH coming soon!
+    </div>
+    <div id="demoLine" class="container-fluid">
+      <div class="row">
+        <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6 hidden-xs">
+          <img class="img-responsive center-block" src="imgs/welcome-invoices-graphic-1.png" alt="Welcome" />
+        </div>
+        <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6">
+          <div class="text-left">
+            <h3>Integrates deeply with<br />Bonsai Contacts</h3>
+            <p>
+              Increase your payment rate with Bonsai
+              Contact: free bulletproof work contracts that
+              are easy to create &cent; e-sign
+            </p>
+            <p>
+              One the contract is signed, generate a
+              payment schedule with one click. <i>Contracts
+              are optional but recommended</i>
+            </p>
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6">
+          <div class="text-left">
+            <h3>Send get paid with<br />beautiful invoices</h3>
+            <p>
+              Create invoices and request payment with the
+              click of th button. Customize them to your
+              brand, then send via email or URL..
+            </p>
+            <p>
+              Clients can pay quickly and securely via credit
+              <i>card. ACH coming soon.</i>
+            </p>
+          </div>
+        </div>
+        <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6 hidden-xs">
+          <img class="img-responsive center-block" src="imgs/welcome-invoices-graphic-2.png" alt="Welcome" />
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6 hidden-xs">
+          <img class="img-responsive center-block" src="imgs/welcome-invoices-graphic-3.png" alt="Welcome" />
+        </div>
+        <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6">
+          <div class="text-left">
+            <h3>Get paid on time<br />with SmartPay</h3>
+            <p>
+              Escrow-like deposits, smarts reminders, soft
+              collections, and much more background
+              magic ensure you're paid on time.
+            </p>
+            <p>
+              Focus on the work you love, not on chasing
+              money. <i>Currently in private beta.</i>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div id="acountInfo" class="container-fluid text-center">
+      Ready to freelance like a pro?<br />
+      Sign up here &amp; get 5 <b>FREE</b> invoices.<br />
+      <button class="btn btn-success">CREATE AN ACCOUNT</button>
+    </div>
+    <div id="copyright" class="container-fluid text-left">
+      <div class="row">
+        <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+          <h4>Products</h4>
+          <ul>
+            <li><a href="#">CONTRACTS</a></li>
+            <li><a href="#">PAYMENTS</a></li>
+            <li><a href="#">RATES</a></li>
+          </ul>
+        </div>
+        <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+          <div class="container">
+            <h4>Company</h4>
+            <ul>
+              <li><a href="#">ABOUT</a></li>
+              <li><a href="#">BLOG</a></li>
+              <li><a href="#">FAQ</a></li>
+            </ul>
+          </div>
+        </div>
+        <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+          <div class="container">
+            <h4>Contact</h4>
+            <ul>
+              <li>720 Market St.. 5th floor</li>
+              <li>San Francisco, CA 94102</li>
+              <li>
+                <span class="glyphicon glyphicon-envelope"></span>
+                <span class="glyphicon glyphicon-asterisk"></span>
+                <span class="glyphicon glyphicon-heart"></span>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+          <p>
+            <img src="imgs/badge_blue.png" src="Copyright" />
+            &copy;2016 Bonsai Inc. Bonsai is not a low firm, does not provide legal services or advice and<br />
+            does not povide or participate in legal representation. <b>Legal</b> - <b>Credits</b>
+          </p>
+        </div>
+      </div>
+    </div>
+    <script src="js/jquery-3.1.1.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html>
